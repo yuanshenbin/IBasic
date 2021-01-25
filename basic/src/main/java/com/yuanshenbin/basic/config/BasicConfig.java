c8ec86aa712f7048238d686590d339c93f05ed91
yuanshenbin <yuan87416471@163.com>
2021-01-25 16:37:13 +0800

    配置分页默认值

---
+++ basic/src/main/java/com/yuanshenbin/basic/config/BasicConfig.java
@@ -0,0 +1,11 @@
+package com.yuanshenbin.basic.config;
+
+/**
+ * author : yuanshenbin
+ * time   : 2021-01-25
+ * desc   : 一些基础配置
+ */
+public class BasicConfig {
+
+    public static int PAGE_SIZE = 20;//分页默认值
+}
